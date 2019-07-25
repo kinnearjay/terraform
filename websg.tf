3e89cc3d5131003425cca3cc7dfde6e7e2d0bbcd
kinnearjay <Ecrofiar@gmail.com>
2019-07-25 15:28:41 +0100

    opened port 8080 for jenkins
    added port 8080 to the webserver security group file for jenkins to communicate on

--- websg.tf
+++ websg.tf
@@ -35,6 +35,12 @@ ingress {
     protocol = "icmp"
     cidr_blocks = ["0.0.0.0/0"] 
     }
+ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
+    }
 
 egress{
       from_port = 22
@@ -60,6 +66,12 @@ egress {
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"] 
     }
+egress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+}
 
 vpc_id = "${aws_vpc.defaultvpc.id}"
 
