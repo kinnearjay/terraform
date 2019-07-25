e0d5dafd754bd5f93300b5bbb8bb6b56783b9c66
kinnearjay <Ecrofiar@gmail.com>
2019-07-25 12:56:11 +0100

    gave the webserver access to port 80
    this fixes the error with yum not being able to update or being able to install anything ;-;

--- websg.tf
+++ websg.tf
@@ -35,6 +35,12 @@ egress{
       protocol = "tcp"
       cidr_blocks= ["0.0.0.0/0"]
   }
+egress{
+      from_port = 80
+      to_port = 80
+      protocol = "tcp"
+      cidr_blocks= ["0.0.0.0/0"]
+  }
 
 vpc_id = "${aws_vpc.defaultvpc.id}"
 
